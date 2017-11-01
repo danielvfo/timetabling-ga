509220db5d67f7e323340b139d647b5c12ccdfb7
Daniel <danielvfo@users.noreply.github.com>
2017-11-01 02:03:08 -0200

    New functions prototypes

--- src/population/Gene.js
+++ src/population/Gene.js
@@ -4,15 +4,31 @@ module.exports = class Gene {
 
   getProfessorsWithPreferences(professors) {
     var temp = [];
-    professors.forEach((item) => {
-      if(item.subject_id) { //Professor has subjects preferences
-        temp.push(item);
+    professors.forEach((professor) => {
+      if(professor.subject_id) { //Professor has subjects preferences
+        temp.push(professor);
       }
     });
     return temp;
   };
 
-  createGene() {
+  hasBond(subject, individual) {
+
+  }
+
+  subtractUnits(subject, professor) {
+
+  };
+
+  isLab(subject) {
+
+  };
+
+  isLab(classroom) {
+
+  };
+
+  sameType(classroom, subject) {
 
   };
 };
